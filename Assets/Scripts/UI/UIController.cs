9458553cc4161f5d38662cd544e672e3a91943a3
Gabriela Bohorquez <gaby030300@hotmail.com>
2023-05-01 02:36:28 -0500

    UI Menu - Fade Transition - Sound Manager

--- Assets/Scripts/UI/UIController.cs
+++ Assets/Scripts/UI/UIController.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIController : MonoBehaviour
 {
     [Header("Map")]
     [SerializeField] GameObject map;
 
+    [Header("Settings")]
+    [SerializeField] GameObject settings;
+
     void Update()
     {
         OpenMap();
+        OpenSettings();
     }
     public void OpenMap()
     {
@@ -26,5 +31,44 @@ public class UIController : MonoBehaviour
             }
 
         }
+    }
+
+    public void OpenSettings()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            settings.SetActive(!settings.gameObject.activeSelf);
+            if (settings.gameObject.activeSelf == true)
+            {
+                Time.timeScale = 0;
+            }
+            else
+            {
+                Time.timeScale = 1;
+            }
+        }
+    }
+    public void OnPlayButton()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void OnOptionsButton()
+    {
+        settings.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public void OnMenuButton()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    public void OnCloseButton()
+    {
+        settings.SetActive(false);
+        Time.timeScale = 1;
+    }
+    public void OnExitButton()
+    {
+        Application.Quit();
     }
 }
